1757cdd1caebea424b773a9d354458a45a98089a
USER <s20035@gsm.hs.kr>
2021-06-13 17:43:30 +0900

    20210613

--- app/src/main/java/com/example/mealbox/MainActivity.kt
+++ app/src/main/java/com/example/mealbox/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mealbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val intent = Intent(this,Splash::class.java)
+            startActivity(intent) // 로딩 액티비티로 화면전환
+        }
     }
-}
--- app/src/main/java/com/example/mealbox/Splash.kt
+++ app/src/main/java/com/example/mealbox/Splash.kt
@@ -5,12 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 
 class Splash : AppCompatActivity() {
-    val time : Long = 1500//로딩 지연시간
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        Handler().postDelayed({
-            finish()
-        },time)
+
     }
 }
